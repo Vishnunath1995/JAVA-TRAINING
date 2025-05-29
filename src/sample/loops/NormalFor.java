70f39dca7e5eb3510f3877293f0d44ea0e61b16e
Vishnunath1995 <javaselenium2024course@gmail.com>
2025-05-29 11:51:02 +0530

    my first java commit

---
+++ src/sample/loops/NormalFor.java
@@ -0,0 +1,16 @@
+package sample.loops;
+
+public class NormalFor {
+
+	public static void main(String[] args) {
+	int i =1;
+	
+	for(i=1;i<=10;i++)
+	{
+		int j=i*3;
+		System.out.println(j);
+	}
+
+	}
+
+}
